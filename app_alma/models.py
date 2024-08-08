88c960d101d27b7b62d9a8663bf6f6254c86ab5d
Duanne Moraes <duanne.souza@gmail.com>
2024-08-08 16:59:33 -0300

    feat:created serilializers

--- app_alma/models.py
+++ app_alma/models.py
@@ -22,7 +22,9 @@ class Student(models.Model):
     payment_status = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.TextField(max_length=20)
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="students"
+    )
 
     def __str__(self) -> str:
         return self.nome
@@ -32,14 +34,18 @@ class StudentProfile(models.Model):
     goal = models.TextField(max_length=255)
     progress = models.TextField(max_length=255)
     feedback = models.TextField(max_length=255)
-    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(
+        Student, on_delete=models.CASCADE, related_name="profiles"
+    )
 
 
 class Payment(models.Model):
     pay_day = models.DateField()
     value = models.DecimalField(decimal_places=2, max_digits=6)
     status = models.BooleanField()
-    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(
+        Student, on_delete=models.CASCADE, related_name="payments"
+    )
 
     def __str__(self) -> str:
         return f"{self.student_id.name} - {self.pay_day}"
@@ -58,9 +64,16 @@ class Coach(models.Model):
 class TrainingSession(models.Model):
     date = models.DateTimeField()
     description = models.TextField(max_length=255)
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
-    coach_id = models.ForeignKey(Coach, on_delete=models.CASCADE)
-    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    place = models.CharField(max_length=15, default="", null=False, blank=False)
+    category_id = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="trainings"
+    )
+    coach_id = models.ForeignKey(
+        Coach, on_delete=models.CASCADE, related_name="trainings"
+    )
+    student_id = models.ForeignKey(
+        Student, on_delete=models.CASCADE, related_name="trainings"
+    )
 
     def __str__(self) -> str:
         return f"{self.student_id.name} - {self.category_id.name} - {self.date}"
