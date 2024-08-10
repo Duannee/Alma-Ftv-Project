fa3a7e98b1a5e163b4c3b3d676a3b2dedce7be91
Duanne Moraes <duanne.souza@gmail.com>
2024-08-10 16:06:11 -0300

    feat: Created Views

--- app_alma/views.py
+++ app_alma/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 
-# Create your views here.
+
+from .models import Account, Category, Student, StudentProfile, Payment, Coach
+from .serializers import (
+    AccountSerializer,
+    CategorySerializer,
+    StudentSerializer,
+    StudentProfileSerializer,
+    PaymentSerializer,
+    CoachSerializer,
+)
+
+
+class ListCreateAccountView(ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class RetrieveUpdateDestroyAccountView(RetrieveUpdateDestroyAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class ListCreateCategoryView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class RetrieveUpdateDestroyCategoryView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ListCreateStudentView(ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class RetrieveUpdateDestroyStudentView(RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class ListCreateStudentProfileView(ListCreateAPIView):
+    queryset = StudentProfile.objects.all()
+    serializer_class = StudentProfileSerializer
+
+
+class RetrieveUpdateDestroyStudentProfileView(RetrieveUpdateDestroyAPIView):
+    queryset = StudentProfile.objects.all()
+    serializer_class = StudentProfileSerializer
+
+
+class ListCreatePaymentView(ListCreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+
+class RetrieveUpdateDestroyPaymentView(RetrieveUpdateDestroyAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+
+class ListCreateCoachView(ListCreateAPIView):
+    queryset = Coach.objects.all()
+    serializer_class = CoachSerializer
+
+
+class RetrieveUpdateDestroyCoachView(RetrieveUpdateDestroyAPIView):
+    queryset = Coach.objects.all()
+    serializer_class = CoachSerializer
