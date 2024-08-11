f1472d3e02da0ee1f5cfc413b0ffe9d0b2e92019
Duanne Moraes <duanne.souza@gmail.com>
2024-08-11 11:19:17 -0300

    feat: Changed the attribute student_id on Payment and StudentProfile Models

--- app_alma/models.py
+++ app_alma/models.py
@@ -14,7 +14,7 @@ class Student(models.Model):
     payment_status = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.TextField(max_length=20)
-    category = models.CharField(max_length=20)
+    category = models.CharField(max_length=20, default="")
 
     def __str__(self) -> str:
         return self.nome
@@ -24,14 +24,14 @@ class StudentProfile(models.Model):
     goal = models.TextField(max_length=255)
     progress = models.TextField(max_length=255)
     feedback = models.TextField(max_length=255)
-    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 
 class Payment(models.Model):
     pay_day = models.DateField()
     value = models.DecimalField(decimal_places=2, max_digits=6)
     status = models.BooleanField()
-    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"{self.student_id.name} - {self.pay_day}"
