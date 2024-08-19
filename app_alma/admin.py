9524d88e89a9c1aea8cf15f9b3f9e7ddff6a5d06
Duanne Moraes <duanne.souza@gmail.com>
2024-08-19 13:58:24 -0300

    feat: Create the admin page with all models

--- app_alma/admin.py
+++ app_alma/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Student, StudentProfile, Payment, Coach
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Student)
+admin.site.register(StudentProfile)
+admin.site.register(Payment)
+admin.site.register(Coach)
