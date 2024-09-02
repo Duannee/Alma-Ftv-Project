29db7bf9843a16d933223390c69d57b2c82e8bf8
Duanne Moraes <duanne.souza@gmail.com>
2024-09-02 15:47:51 -0300

    fix: Change settings.py

--- project_alma/settings.py
+++ project_alma/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config, Csv
 from dj_database_url import parse as db_url
 import dj_database_url
 import os
