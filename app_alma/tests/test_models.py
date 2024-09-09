cb264d0588c7837a81a79f4b37a65c070a2bb468
Duanne Moraes <duanne.souza@gmail.com>
2024-09-09 16:05:12 -0300

    feat: Create tests for Student Profile Model

--- app_alma/tests/test_models.py
+++ app_alma/tests/test_models.py
@@ -32,3 +32,32 @@ class ModelStudentTestCase(TestCase):
         self.assertEqual(self.student.email, "studentest@mail.com")
         self.assertEqual(self.student.phone, "21 99999-9999")
         self.assertEqual(self.student.category, "test category")
+
+
+class ModelStudentProfileTestCase(TestCase):
+    def setUp(self):
+        self.student = Student.objects.create(
+            name="Student test",
+            age="26",
+            email="studentest@mail.com",
+            phone="21 99999-9999",
+            category="test category",
+        )
+        self.student_profile = StudentProfile.objects.create(
+            student=self.student,
+            goal="test goal",
+            progress="test progress",
+            feedback="test feedback",
+        )
+
+    def test_verify_student_profile_attribute(self):
+        """Test for verify STUDENT PROFILE attributes"""
+        self.assertEqual(self.student_profile.student, self.student)
+        self.assertEqual(self.student_profile.goal, "test goal")
+        self.assertEqual(self.student_profile.progress, "test progress")
+        self.assertEqual(self.student_profile.feedback, "test feedback")
+
+    def test_profile_delete_on_student_delete(self):
+        """Test for verify if student profile will be delete when student will be delete"""
+        self.student.delete()
+        self.assertFalse(Student.objects.filter(pk=self.student.id).exists())
