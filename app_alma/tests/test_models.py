776af631359ca11e84da4b70ac39094158518683
Duanne Moraes <duanne.souza@gmail.com>
2024-09-09 15:48:42 -0300

    feat: Create test for Student Model

--- app_alma/tests/test_models.py
+++ app_alma/tests/test_models.py
@@ -10,6 +10,25 @@ class ModelUserTestCase(TestCase):
         )
 
     def test_verify_user_attribute(self):
-        """Test for verify user attributes"""
+        """Test for verify USER attributes"""
         self.assertEqual(self.user.username, "user test")
         self.assertEqual(self.user.email, "usertest@mail.com")
+
+
+class ModelStudentTestCase(TestCase):
+    def setUp(self):
+        self.student = Student.objects.create(
+            name="Student test",
+            age="26",
+            email="studentest@mail.com",
+            phone="21 99999-9999",
+            category="test category",
+        )
+
+    def test_verify_student_attribute(self):
+        """Test for verify STUDENT attributes"""
+        self.assertEqual(self.student.name, "Student test")
+        self.assertEqual(self.student.age, "26")
+        self.assertEqual(self.student.email, "studentest@mail.com")
+        self.assertEqual(self.student.phone, "21 99999-9999")
+        self.assertEqual(self.student.category, "test category")
