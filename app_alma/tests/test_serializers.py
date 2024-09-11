3ab5099946c7309cdfca9ecb3d40ddb34ca2caca
Duanne Moraes <duanne.souza@gmail.com>
2024-09-11 16:06:33 -0300

    feat: Create test for User serializer

---
+++ app_alma/tests/test_serializers.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from app_alma.models import User, Student, StudentProfile, Payment, Coach
+from app_alma.serializers import (
+    UserSerializer,
+    StudentSerializer,
+    StudentProfileSerializer,
+    PaymentSerializer,
+    CoachSerializer,
+)
+
+
+class SerializerUserTestCase(TestCase):
+    def setUp(self):
+        self.user = User(
+            username="user test",
+            email="usertest@mail.com",
+        )
+        self.user_serializer = UserSerializer(instance=self.user)
+
+    def test_verify_User_fields_serialized(self):
+        """Test to verify if User fields are serialized"""
+        data = self.user_serializer.data
+        self.assertEqual(set(data.keys()), set(["id", "username", "email"]))
+
+    def test_verify_content_User_fields_serializer(self):
+        """Test to verify the content into the fields User serializer"""
+        data = self.user_serializer.data
+        self.assertEqual(data["username"], self.user.username)
+        self.assertEqual(data["email"], self.user.email)
