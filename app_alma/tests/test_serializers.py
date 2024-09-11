05eedfb0cb04ef925c0711040b7f00579a3aece4
Duanne Moraes <duanne.souza@gmail.com>
2024-09-11 16:17:02 -0300

    feat: Create test for Student Serializer

--- app_alma/tests/test_serializers.py
+++ app_alma/tests/test_serializers.py
@@ -27,3 +27,31 @@ class SerializerUserTestCase(TestCase):
         data = self.user_serializer.data
         self.assertEqual(data["username"], self.user.username)
         self.assertEqual(data["email"], self.user.email)
+
+
+class SerializerStudentTestCase(TestCase):
+    def setUp(self):
+        self.student = Student(
+            name="Student test",
+            age=26,
+            email="studentest@mail.com",
+            phone="21 99999-9999",
+            category="test category",
+        )
+        self.student_serializer = StudentSerializer(instance=self.student)
+
+    def test_verify_Student_fields_serialized(self):
+        """Test to verify if Student fields are serialized"""
+        data = self.student_serializer.data
+        self.assertEqual(
+            set(data.keys()), set(["id", "name", "age", "email", "phone", "category"])
+        )
+
+    def test_verify_content_Student_fields_serializer(self):
+        """Test to verify the content into the fields Student serializer"""
+        data = self.student_serializer.data
+        self.assertEqual(data["name"], self.student.name)
+        self.assertEqual(data["age"], self.student.age)
+        self.assertEqual(data["email"], self.student.email)
+        self.assertEqual(data["phone"], self.student.phone)
+        self.assertEqual(data["category"], self.student.category)
