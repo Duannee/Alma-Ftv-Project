74fad18ddfcad5ac4f730bb58c6e874fdbd60705
Duanne Moraes <duanne.souza@gmail.com>
2024-09-20 11:08:52 -0300

    feat:Create test to Student Profile ID get request

--- app_alma/tests/test_get_student_profile.py
+++ app_alma/tests/test_get_student_profile.py
@@ -1,4 +1,4 @@
-from app_alma.models import User, Student
+from app_alma.models import User, Student, StudentProfile
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -14,9 +14,23 @@ class GetStudentProfileTestCase(APITestCase):
             phone="99 99999-9999",
             category="test category",
         )
+        self.student_profile = StudentProfile.objects.create(
+            student=self.student,
+            goal="test goal",
+            progress="test progress",
+            feedback="test feedback",
+        )
 
     def test_USER_GET_request(self):
         """Get request test for Student Profile"""
         self.url = reverse("student-profile-list")
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_USER_GET_ID_request(self):
+        """Get ID request test for Student Profile"""
+        self.url = reverse(
+            "student-profile-id-list", kwargs={"pk": self.student_profile.pk}
+        )
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
