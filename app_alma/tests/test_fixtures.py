d5fa0d7ba34cc6e2f754e3ea476c654c80bc3065
Duanne Moraes <duanne.souza@gmail.com>
2024-09-22 10:38:12 -0300

    refactor: Add all models in test fixtures loading

--- app_alma/tests/test_fixtures.py
+++ app_alma/tests/test_fixtures.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from app_alma.models import User, Student
+from app_alma.models import User, Student, StudentProfile, Payment, Coach
 
 
 class FixturesTestCase(TestCase):
@@ -9,5 +9,11 @@ class FixturesTestCase(TestCase):
         """Test to verify fixtures loading"""
         user = User.objects.get(username="duanne")
         student = Student.objects.get(pk=2)
+        student_profile = StudentProfile.objects.get(pk=6)
+        payment = Payment.objects.get(pk=5)
+        coach = Coach.objects.get(pk=2)
         self.assertEqual(user.email, "duanne@mail.com")
         self.assertEqual(student.category, "Avancado")
+        self.assertEqual(student_profile.goal, "Ser excelente")
+        self.assertEqual(payment.status, "Pending")
+        self.assertEqual(coach.name, "Caio")
