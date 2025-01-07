caa1c20caff08508e291596530fb2acb805d7ed8
Duanne Moraes <duanne.souza@gmail.com>
2025-01-07 18:03:13 -0300

    fix: Settings error

--- project_alma/settings.py
+++ project_alma/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": "dpg-crb02h56l47c73d1gk0g-a",
+        "HOST": "dpg-ctupc1popnds73fp7bl0-a",
         "PORT": 5432,
     },
     "sqlite3_db": {
